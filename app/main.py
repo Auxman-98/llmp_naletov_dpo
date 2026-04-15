b59e58cde03b7528cd5804154d40e24592349b71
Artyom Nalyotov <whalewander@yandex.ru>
2026-04-15 13:16:35 +0200

    Start the app preconfigured through dotenv

--- app/main.py
+++ app/main.py
@@ -1,10 +1,5 @@
 from fastapi import FastAPI
 
-from app.core.config import settings
+from .core.config import settings
 
-def main():
-    app = FastAPI(title=settings["APP_NAME"])
-
-
-if __name__ == "__main__":
-    main()
+app = FastAPI(title=settings.app_name)
