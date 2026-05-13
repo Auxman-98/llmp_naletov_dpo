d492871d4788c9c3d9d2957da460c11d7ccca075
Artyom Nalyotov <whalewander@yandex.ru>
2026-05-13 14:40:31 +0200

    Fix entrypoint structure

--- app/main.py
+++ app/main.py
@@ -5,10 +5,5 @@ from app.core.config import settings
 from app.api.routes_auth import router as auth_router
 
 
-async def main():
-    app = FastAPI(title=settings.app_name)
-    app.include_router(auth_router)
-
-
-if __name__ == "__main__":
-    asyncio.run(main())
+app = FastAPI(title=settings.app_name)
+app.include_router(auth_router)
