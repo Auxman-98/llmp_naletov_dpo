a8cf6adcb776f04d461840b701778556bd505b24
Artyom Nalyotov <whalewander@yandex.ru>
2026-05-20 19:46:54 +0200

    Explicitly create new db

--- app/main.py
+++ app/main.py
@@ -1,8 +1,24 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 
 from app.core.config import settings
+from app.db.session import engine
+from app.db.base import Base
+
 from app.api.routes_auth import router as auth_router
 
 
-app = FastAPI(title=settings.app_name)
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+    yield
+
+
+app = FastAPI(
+    title=settings.app_name,
+    lifespan=lifespan,
+)
 app.include_router(auth_router)
